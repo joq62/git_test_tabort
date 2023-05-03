7fd3b1aa9a991bfddb35b935836914dcc3c5e3ed
joq62 <jakim.leche@gmail.com>
2023-05-03 05:20:41 +0200

    added git_push.erl

---
+++ git_push.erl
@@ -0,0 +1,21 @@
+-module(git_push).
+-export([push_file/3]).
+
+push_file(FilePath, RepoUrl, AccessToken) ->
+    {ok, FileContent} = file:read_file(FilePath),
+    FileName = filename:basename(FilePath),
+    io:format("FileName ~p~n",[FileName]),
+  %  Url = RepoUrl ++ "/contents/" ++ FileName,
+    Url = RepoUrl ++"/"++FileName,
+    io:format("Url ~p~n",[Url]),
+  
+
+    Body = #{ "message" => "add " ++ FileName,
+              "content" => base64:encode(FileContent) },
+    io:format("Body ~p~n",[Body]),
+    Headers = [{"Authorization", "token " ++ AccessToken},{"User-Agent", "joq62"}],
+    io:format("Headers ~p~n",[Headers]),
+%    Headers = [{"Authorization", "token " ++ AccessToken},{"User-Agent", "Erlang"}],
+
+    {ok, Response} = httpc:request(post, {Url, Headers, "application/json", Body}, [], []),
+    Response.
